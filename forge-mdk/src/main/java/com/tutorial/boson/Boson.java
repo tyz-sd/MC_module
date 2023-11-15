321eb257d570c32cf32478b76b4830168390ed88
tyz-sd <tiz026@ucsd.edu>
2023-11-14 18:42:27 -0800

    debug

---
+++ forge-mdk/src/main/java/com/tutorial/boson/Boson.java
@@ -0,0 +1,7 @@
+package com.tutorial.boson;
+
+public class Boson {
+
+
+}
+
---
+++ forge-mdk/src/main/java/com/tutorial/boson/Utils.java
@@ -0,0 +1,7 @@
+package com.tutorial.boson;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class Utils {
+    public static final String MOD_ID = "boson";
+}
